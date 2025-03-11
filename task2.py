9859c603a0afda93b3a097e9fad3d73ef9498195
Vorobyov Kirill <vorobyovk@gmail.com>
2025-03-11 10:59:24 +0100

    fix bug in task 2

--- task2.py
+++ task2.py
@@ -2,13 +2,16 @@ import random
 
 def get_numbers_ticket(min, max, quantity): # define function get_numbers_ticket with 3 arguments
     numbers = []
-    while len(numbers) < quantity: # check if the length of the list is less than the quantity
-        number = random.randint(min, max)
-        if number not in numbers: # check if the number is not in the list
-            numbers.append(number)
-    numbers=sorted(numbers) # sort the our list
-    return numbers 
+    if max - min < quantity:
+        return "Кількість чисел більша за діапазон чисел"
+    else:
+        while len(numbers) < quantity: # check if the length of the list is less than the quantity
+            number = random.randint(min, max)
+            if number not in numbers: # check if the number is not in the list
+                numbers.append(number)
+        numbers=sorted(numbers) # sort the our list
+        return numbers 
 
-lottery_numbers = get_numbers_ticket(1, 49, 6) # call the function with the arguments
+lottery_numbers = get_numbers_ticket(100, 150, 10) # call the function with the arguments
 print(f"Ваші лотерейні числа: {lottery_numbers}") # output the result of the function
 
